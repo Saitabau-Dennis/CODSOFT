bf8d2b2d06f3952e3617563736c85e003349200c
Saitabau-Dennis <dennisntete28@gmail.com>
2024-07-08 00:33:13 +0300

    Refactor navigation menu in index.html and styles.css for responsive design

--- Tribute/script.js
+++
@@ -1,9 +0,0 @@
-
-    const mobileMenu = document.getElementById('mobile-menu');
-    const navList = document.querySelector('.nav-list');
-
-    mobileMenu.addEventListener('click', () => {
-        mobileMenu.classList.toggle('toggle');
-        navList.classList.toggle('show');
-    });
-
